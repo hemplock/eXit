fe6e052b075d978fabf128a2dc03380ea05676f5
hemplock <37376768+hemplock@users.noreply.github.com>
2018-08-25 21:04:37 -0600

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,124 @@
+<?php return array (
+  'barryvdh/laravel-ide-helper' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\LaravelIdeHelper\\IdeHelperServiceProvider',
+    ),
+  ),
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'barryvdh/laravel-debugbar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Debugbar\\ServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Debugbar' => 'Barryvdh\\Debugbar\\Facade',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'creativeorange/gravatar' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Creativeorange\\Gravatar\\GravatarServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Gravatar' => 'Creativeorange\\Gravatar\\Facades\\Gravatar',
+    ),
+  ),
+  'jenssegers/date' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Jenssegers\\Date\\DateServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Date' => 'Jenssegers\\Date\\Date',
+    ),
+  ),
+  'prologue/alerts' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Prologue\\Alerts\\AlertsServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Alert' => 'Prologue\\Alerts\\Facades\\Alert',
+    ),
+  ),
+  'backpack/base' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Backpack\\Base\\BaseServiceProvider',
+    ),
+  ),
+  'backpack/crud' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Backpack\\CRUD\\CrudServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'CRUD' => 'Backpack\\CRUD\\CrudServiceProvider',
+    ),
+  ),
+  'backpack/generators' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Backpack\\Generators\\GeneratorsServiceProvider',
+    ),
+  ),
+  'laracasts/generators' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laracasts\\Generators\\GeneratorsServiceProvider',
+    ),
+  ),
+  'barryvdh/laravel-elfinder' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Barryvdh\\Elfinder\\ElfinderServiceProvider',
+    ),
+  ),
+);
